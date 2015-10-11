d7972effc3183d8b5ae4b635318a8a61e04d15ae
Krivenko Petr <krivenkopetr99@gmail.com>
2015-10-11 23:27:29 +0300

    repr.c

--- courses/prog_base/tasks/long2double/repr.c
+++ courses/prog_base/tasks/long2double/repr.c
@@ -4,23 +4,19 @@
 int main(){
 long long number;
 short S,i,j;
-double F=0,V;
-int E=0;
+double F=0,V,E=0;
 
 printf("Enter the long long number ");
 scanf("%lld", &number);
 
 S=(number>>63)&1;
 for (j=52;j<63;++j) {
-        E+=((number>>j)&1)*(pow(2,(j-52)));// Ось цей дивний цикл:)
+        E+=((number>>j)&1)*(pow(2,(j-52)));
 }
-//printf("S=%d\n",S);
-//printf("E=%d\n",E);
 
 for (i=51;i>=0;--i) {
 	F+=(((number>>i)&1)*(pow(2,i-52)));
 }
-//printf("F=%.325llf\n",F);
 if ((E==2047) && F!=0) {
 printf("V=NaN");
 }
@@ -46,6 +42,5 @@ if ((E==0) && (F==0) && (S==0)){
 	V=0;
 	printf("V=%.325llf",V);
 }
-
 }
 
