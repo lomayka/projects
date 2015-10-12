ac1073c3c1c3b3530eb2c5fc6f83fe58583c074f
Krivenko Petr <krivenkopetr99@gmail.com>
2015-10-12 17:57:30 +0300

    repr.c

--- courses/prog_base/tasks/long2double/repr.c
+++ courses/prog_base/tasks/long2double/repr.c
@@ -1,18 +1,16 @@
-/*#include <stdio.h>
+#include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 double long2double (long long);
 int main(){
 long long number;
-double result;
-result=long2double(number);
-return result;
-}*/
+printf("%.325llf",long2double(number));
+}
 double long2double (long long number){
 short S,i,j;
 double F=0,V,E=0;
 printf("Enter the long long number ");
-scanf("%lld", &number);
+scanf("%lli", &number);
 S=(number>>63)&1;
 for (j=52;j<63;++j) {
         E+=((number>>j)&1)*(pow(2,(j-52)));
@@ -22,28 +20,26 @@ for (i=51;i>=0;--i) {
 	F+=(((number>>i)&1)*(pow(2,i-52)));
 }
 if ((E==2047) && F!=0) {
-printf("V=NaN");
+return NAN;
 }
 if ((E==2047) && (F==0) && (S==1)) {
-	printf("V=-Infinity");
+	return -INFINITY;
 	}
 if ((E==2047) && (F==0) && (S==0)) {
-	printf("V=Infinity");
+	return INFINITY;
 	}
 if ((E>0) && (E<2047)){
 V=pow(-1,S)*pow(2,E-1023)*(1+F);
-printf("V=%.325llf",V);
+return V;
 }
 if ((E==0) && (F!=0)) {
 	V=pow(-1,S)*pow(2,-1022)*F;
-	printf("V=%.325llf",V);
+	return V;
 }
 if ((E==0) && (F==0) && (S==1)){
-	V=-0;
-	printf("V=%.325llf",V);
+	return -0;
 }
 if ((E==0) && (F==0) && (S==0)){
-	V=0;
-	printf("V=%.325llf",V);
+	return 0;
 }
 }
