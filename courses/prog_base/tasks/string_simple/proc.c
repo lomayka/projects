f9efb6407ef310fb8132a08b22a16566ba45a302
Krivenko Petr <krivenkopetr99@gmail.com>
2015-11-01 19:47:10 +0200

    proc.c

---
+++ courses/prog_base/tasks/string_simple/proc.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+char *process (char *resultStr, const char *textLines [], int linesNum, const char *extraStr) {
+    int maxCounter = 0;
+    int maxIndex = 0;
+    int maxLength = 0;
+    int counter = 0;
+    int length=0;
+    int i=0;
+    int j=0;
+
+    for (i = 0; i < linesNum; i++) {
+        for (j = 0; j < linesNum; j++) {
+            if (strcmp(textLines[i], textLines[j]) == 0) {
+                counter++;
+                length = strlen(textLines[i]);
+            }
+        }
+        if (counter > maxCounter) {
+            maxCounter = counter;
+            maxIndex = i;
+        }
+        if (counter==maxCounter){
+        if (length > maxLength){
+            maxLength = length;
+            maxIndex = i;
+            }
+        }
+
+    }
+
+    char tempResult[100];
+    sprintf(tempResult, "%d, %s", maxCounter, textLines[maxIndex]);
+    resultStr = tempResult;
+    return resultStr;
+}
