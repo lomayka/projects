e5430132e64ac543cbb15891894a7f92135b3a10
Krivenko Petr <krivenkopetr99@gmail.com>
2015-12-06 16:24:17 +0200

    struct

--- courses/prog_base/tasks/struct/main.c
+++ courses/prog_base/tasks/struct/main.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 
-struct Factories {
+struct Factory {
 int amount;
 char name[20];
 char best_worker[20];
 };
 
 
-int count(int size, struct Factories arr[size]){
+int count(int size, struct Factory arr[size]){
     int cnt = 0;
     for (int i = 0; i < size; i++){
         if (((arr+i)->amount)/* or arr[i].amount*/ > 50){
@@ -18,10 +18,10 @@ int count(int size, struct Factories arr[size]){
     }
     return cnt;
 }
-void change(struct Factories * pVar, const char * newValue){
+void change(struct Factory * pVar, const char * newValue){
     strcpy(pVar->name, newValue);
 }
-void print(struct Factories * pVar){
+void print(struct Factory * pVar){
     for (int i = 0; i < 4; i++){
     printf("Factory %d:\n Name: %s;\n Workers: %d;\n Worker of month: %s;\n",i,(pVar+i)->name,(pVar+i)->amount,(pVar+i)->best_worker);
 
@@ -32,10 +32,9 @@ void print(struct Factories * pVar){
 int main()
 {
    srand(time(NULL));
-   struct Factories arr[4] = {{rand()%100+1, "Dell", "Tom Johns"},{rand()%100+1,"Asus", "Peter Cooper"}, {rand()%100+1,"MSI", "Henry Parker"}, {rand()%100+1,"HP", "Steve MacGee"}};
+   struct Factory arr[4] = {{rand()%100+1, "Dell", "Tom Johns"},{rand()%100+1,"Asus", "Peter Cooper"}, {rand()%100+1,"MSI", "Henry Parker"}, {rand()%100+1,"HP", "Steve MacGee"}};
     int N = 0;
-    char * newValue;
-
+    char * newValue = (char*)malloc(30*sizeof(char));
     print(arr);
     printf("\n");
     printf("More than 50 workers in %d factories; \n",count(4,arr));
@@ -45,7 +44,7 @@ int main()
     change(&arr[N],newValue);
     printf("\n");
     printf("Renamed factory number %d:\n Name: %s;\n Workers: %d;\n Worker of month: %s;\n",N,(arr+N)->name,(arr+N)->amount,(arr+N)->best_worker);
-
+    free(newValue);
     return 0;
 }
 
