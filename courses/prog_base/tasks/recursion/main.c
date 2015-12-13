84b68b3a50a417db95a31a2043513cfac0b76e78
Krivenko Petr <krivenkopetr99@gmail.com>
2015-12-13 13:00:25 +0200

    recursion

---
+++ courses/prog_base/tasks/recursion/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+int lastnspace(const char * pf, const char * pc) {
+    if (pf > pc)         return -1;
+    else if (0 == isspace(*pc))   return (int)*pc;
+    else                        return lastnspace(pf, pc - 1);
+}
+
+int firstnspace(const char * pf, const char * pc) {
+    if (pf > pc)         return -1;
+    else if (0 == isspace(*pf))   return (int)*pf;
+    else                        return firstnspace(pf+1, pc);
+}
+
+int main(){
+char * str = "  Batman  ";
+printf("%i", firstnspace(str, str + strlen(str) - 1)- lastnspace(str, str + strlen(str) - 1));
+return 0;
+}
+
+
