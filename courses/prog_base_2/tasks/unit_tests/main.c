62d1c7ba617f098ee110d99e43ec9481e120d928
kpo99 <krivenkopetr99@gmail.com>
2016-03-06 19:12:57 +0200

    unit_tests

--- courses/prog_base_2/tasks/unit_tests/main.c
+++ courses/prog_base_2/tasks/unit_tests/main.c
@@ -20,29 +20,37 @@ static void new_void_listAddDelete(void **state)
     assert_int_equal(list_delete(ls1,5),WRONG_INDEX);
     assert_int_equal(list_delete(ls1,0),SUCCESSFUL);
     assert_int_equal(list_add(ls1,9,3),WRONG_INDEX);
-    list_free(ls1);
+    assert_int_equal(list_free(ls1),SUCCESSFUL);
 }
 
 static void new_void_listSizeCheck(void **state)
 {
     list_t * ls3 = list_new();
     assert_int_equal(list_size(ls3),0);
-    assert_int_equal(list_add(ls3,MIN,0),SUCCESSFUL);
-    assert_int_equal(list_add(ls3,ADD,1),SUCCESSFUL);
+    list_add(ls3,MIN,0);
+    list_add(ls3,ADD,1);
     assert_int_equal(list_check(ls3,1),ADD);
     assert_int_equal(list_size(ls3),2);
-   list_free(ls3);
+    list_free(ls3);
 }
 
-static void new_void_listMaxMin(void **state)
+static void new_void_listMax(void **state)
 {
     list_t * ls4 = list_new();
-    assert_int_equal(list_add(ls4,ADD,0),SUCCESSFUL);
-    assert_int_equal(list_add(ls4,MAX,1),SUCCESSFUL);
-    assert_int_equal(list_add(ls4,MIN,2),SUCCESSFUL);
+    list_add(ls4,ADD,0);
+    list_add(ls4,MAX,1);
+
     assert_int_equal(list_max(ls4),MAX);
+    list_free(ls4);
+}
+
+static void new_void_listMin(void **state)
+{
+    list_t * ls4 = list_new();
+    list_add(ls4,ADD,0);
+    list_add(ls4,MIN,1);
     assert_int_equal(list_min(ls4),MIN);
-    assert_int_equal(list_free(ls4),SUCCESSFUL);
+    list_free(ls4);
 }
 
 
@@ -52,7 +60,8 @@ int main(void) {
     {
         cmocka_unit_test(new_void_listAddDelete),
         cmocka_unit_test(new_void_listSizeCheck),
-        cmocka_unit_test(new_void_listMaxMin),
+        cmocka_unit_test(new_void_listMax),
+        cmocka_unit_test(new_void_listMin),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
